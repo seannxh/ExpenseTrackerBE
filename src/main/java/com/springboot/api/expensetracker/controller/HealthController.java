529c60dafd64d3d382548d00eb05e13951ededcb
seannxh <nohsean6@gmail.com>
2025-06-22 14:58:50 -0500

    / Healthchecker created

---
+++ src/main/java/com/springboot/api/expensetracker/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.springboot.api.expensetracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "✅ Backend is running!";
+    }
+}
